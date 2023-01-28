605f016c06d69eebeceae05c3611c31895c0cf39
Marcin Torba <torbamarcin@gmail.com>
2023-01-28 15:54:19 +0100

    Move to nano DI framework.

--- src/MakoIoT.Device/IoTDevice.cs
+++ src/MakoIoT.Device/IoTDevice.cs
@@ -1,24 +1,43 @@
 ﻿using System;
-using MakoIoT.Device.Services.DependencyInjection;
 using MakoIoT.Device.Services.Interface;
 
 namespace MakoIoT.Device
 {
     public class IoTDevice : IDevice
     {
+        public IServiceProvider ServiceProvider { get; }
+
         public event EventHandler Starting;
         public event EventHandler Stopped;
 
+        public IoTDevice(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
         public void Start()
         {
-            if (DI.IsRegistered(typeof(IDeviceStartBehavior)))
+            if (IsRegistered(ServiceProvider, typeof(IDeviceStartBehavior)))
             {
-                var behavior = (IDeviceStartBehavior)DI.Resolve(typeof(IDeviceStartBehavior));
+                var behavior = (IDeviceStartBehavior)ServiceProvider.GetService(typeof(IDeviceStartBehavior));
                 if (!behavior.DeviceStarting())
+                {
                     return;
-                behavior = null;
+                }
             }
             Starting?.Invoke(this, EventArgs.Empty);
+        }
+
+        // TODO: Tests
+        public static bool IsRegistered(IServiceProvider serviceProvider, Type serviceType)
+        {
+            var service = serviceProvider.GetService(serviceType);
+            if (service == null)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public void Stop()
